677a9b1eb170821eeae020ee0973aecf177caef4
adityarifkiy16 <adityarifkistemba@gmail.com>
2025-07-11 11:29:57 +0700

    fix : path

--- app/Http/Controllers/TProductController.php
+++ app/Http/Controllers/TProductController.php
@@ -401,10 +401,10 @@ class TProductController extends Controller
             $product = TProduct::with('category', 'category.jenis')->find($request->query('id'));
             $arr['product'] = $product;
             $convertedImgs = [];
-            $photopath = public_path('storage/' . $product->photo);
+            $photopath = storage_path('app/public/' . $product->photo);
             if (file_exists($photopath) && Str::endsWith($product->photo, '.webp')) {
                 $jpgName = Str::replaceLast('.webp', '.jpg', $product->photo);
-                $jpgPath = public_path('storage/temp_images/' . $jpgName);
+                $jpgPath = storage_path('app/public/temp_images/' . $jpgName);
                 $directory = dirname($jpgPath);
                 if (!file_exists($directory)) {
                     mkdir($directory, 0755, true);
