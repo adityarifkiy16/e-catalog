9f3e6120d9c514ddf96bbc2d453555358123af71
adityarifkiy16 <adityarifkistemba@gmail.com>
2025-07-14 10:47:40 +0700

    fix : add max size

--- app/Http/Controllers/TProductController.php
+++ app/Http/Controllers/TProductController.php
@@ -167,7 +167,7 @@ class TProductController extends Controller
             ],
             'name' => 'nullable|string|max:255',
             'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg,webp|max:2048',
-            'image-mockup' => 'nullable|array',
+            'image-mockup' => 'nullable|array|max:5',
             'image-mockup.*' => 'image|mimes:jpeg,png,jpg,gif,svg,webp|max:5024',
             'category_id' => 'required|exists:m_categories,id',
         ]);
