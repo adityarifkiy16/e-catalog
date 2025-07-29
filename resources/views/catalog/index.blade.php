089cac7f2ef8944c9eeca5cc38f0407719843126
adityarifkiy16 <adityarifkistemba@gmail.com>
2025-07-29 16:57:11 +0700

    fix : change 3d

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -174,8 +174,8 @@
             @foreach ($jenis as $key => $item)
                 <div class="col-6 col-sm-4 col-md-2 text-center mb-4 product-card" data-id="{{ $item->id }}">
                     <div class="border-0 h-100 pointer d-flex flex-column justify-content-center align-items-center">
-                        <img src="{{ asset('dist/img/product/' . $key . '.png') }} ?v{{ time() }}"
-                            class="img-fluid w-100" alt="{{ $item->name }}">
+                        <img src="{{ asset('dist/img/product/' . $key . '.png') }}" class="img-fluid w-100"
+                            alt="{{ $item->name }}">
                         <div class="card-body mt-0">
                             <h4 class="card-text font-weight-bold">{{ $item->name }}</h4>
                             <p class="card-text text-muted">{{ $item->products_count }} Product</p>
