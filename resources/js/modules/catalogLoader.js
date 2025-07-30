114bc047d712842ecaef8fcfe3d6a9346f085931
adityarifkiy16 <adityarifkistemba@gmail.com>
2025-07-30 16:34:42 +0700

    fix : remove tipe if wallpanel

--- resources/js/modules/catalogLoader.js
+++ resources/js/modules/catalogLoader.js
@@ -99,7 +99,7 @@ function updateCategoryMenu(response) {
             $('#category-menu-item-label, #category-modal-item-label').html('Motif');
             break;
         case 'Wallpanel':
-            $('#category-menu-item-label, #category-modal-item-label').html('Tipe');
+            $('#category-menu-item-label, #category-modal-item-label').addClass('d-none');
             break;
         default:
             $('#category-menu-item-label, #category-modal-item-label').html('Kategori');
