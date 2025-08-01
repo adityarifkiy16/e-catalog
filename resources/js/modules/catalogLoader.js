63b7c87988cd8923529449978a189dd62a1f53e0
adityarifkiy16 <adityarifkistemba@gmail.com>
2025-08-01 11:12:18 +0700

    fix : handling race condition

--- resources/js/modules/catalogLoader.js
+++ resources/js/modules/catalogLoader.js
@@ -14,69 +14,59 @@ export function setCatalogConfig(config) {
     selectedJenis = config.selectedJenis;
     category = config.category ?? null;
 }
-
-/**
- * Load more products from server and render them.
- * This function will be called when user reaches the bottom of the page.
- * If the data is successfully loaded, it will render the products and update the current page.
- * If the data is failed to load, it will show an error message.
- * If the data is empty, it will show a message that there is no more data.
- */
 export function loadMoreData() {
-    if (!category || category === 'null' || category === '') {
-        $('#btn-download').addClass('d-none');
-    } else {
-        $('#btn-download').removeClass('d-none');
-    }
-
-    if (isLoading || lastPage) return;
-    isLoading = true;
-    showLoading();
-
-    const search = $('#search-input').val();
-    $.ajax({
-        url: `/catalog`,
-        type: 'GET',
-        data: {
-            page: currentPage,
-            search,
-            jenis: selectedJenis,
-            category
-        },
-        success: function (response) {
-            const products = response.data.data ?? [];
-            if (products.length > 0) {
-                renderProducts(products, selectedJenis);
-                currentPage++;
-                if (currentPage > response.data.last_page) lastPage = true;
-            } else {
-                if (currentPage === 1) {
-                    $('#mockup').addClass('d-none');
-                    $('#product-list .row').append(
-                        `<div class="col-12"><img src="dist/img/no-data.png" alt="no-data"
-                            class="img-fluid mx-auto d-block" style="max-width: 100%; height: auto; margin-top: 100px; margin-bottom: 100px;"></div>`
-                    );
-                }
-                lastPage = true;
-            }
-            updateCategoryMenu(response);
-            isLoading = false;
-        },
-        error: function () {
-            isLoading = false;
-            console.log('Gagal memuat data.');
-        },
-        complete: function () {
-            isLoading = false;
-            hideLoading();
+    return new Promise((resolve, reject) => {
+        if (!category || category === 'null' || category === '') {
+            $('#btn-download').addClass('d-none');
+        } else {
+            $('#btn-download').removeClass('d-none');
         }
+
+        if (isLoading || lastPage) return resolve(); // tetap resolve untuk menghindari deadlock
+        isLoading = true;
+        showLoading();
+
+        const search = $('#search-input').val();
+        $.ajax({
+            url: `/catalog`,
+            type: 'GET',
+            data: {
+                page: currentPage,
+                search,
+                jenis: selectedJenis,
+                category
+            },
+            success: function (response) {
+                const products = response.data.data ?? [];
+                if (products.length > 0) {
+                    renderProducts(products, selectedJenis);
+                    currentPage++;
+                    if (currentPage > response.data.last_page) lastPage = true;
+                } else {
+                    if (currentPage === 1) {
+                        $('#mockup').addClass('d-none');
+                        $('#product-list .row').append(
+                            `<div class="col-12"><img src="dist/img/no-data.png" alt="no-data"
+                                class="img-fluid mx-auto d-block" style="max-width: 100%; height: auto; margin-top: 100px; margin-bottom: 100px;"></div>`
+                        );
+                    }
+                    lastPage = true;
+                }
+                updateCategoryMenu(response);
+                resolve();
+            },
+            error: function () {
+                console.log('Gagal memuat data.');
+                reject();
+            },
+            complete: function () {
+                isLoading = false;
+                hideLoading();
+            }
+        });
     });
 }
 
-/**
- * Update category menu based on the given response and selected jenis
- * @param {object} response - response from the server
- */
 function updateCategoryMenu(response) {
     const categories = response.jenis?.categories ?? [];
     const name = response.jenis?.name;
@@ -87,10 +77,13 @@ function updateCategoryMenu(response) {
     } else {
         $('#category-container').removeClass('d-none');
     }
-    if (data.category.images.length === 0) {
-        $('#mockup').addClass('d-none');
+
+    if (category) {
+        if (data.category.images.length === 0) {
+            $('#mockup').addClass('d-none');
+        }
+        renderMockup(data.category.images, selectedJenis, uniquePaths);
     }
-    renderMockup(data.category.images, selectedJenis, uniquePaths);
 
     switch (name) {
         case 'PVC Board':
@@ -134,11 +127,6 @@ function updateCategoryMenu(response) {
     }
 }
 
-/**
- * Reset state of catalog loader.
- * This function will reset the state of catalog loader to its initial state.
- * It will reset the current page to 1, set isLoading to false, set lastPage to false, and clear the product list.
- */
 export function resetState() {
     currentPage = 1;
     isLoading = false;
--- resources/js/modules/filter.js
+++ resources/js/modules/filter.js
@@ -1,16 +1,27 @@
 import { resetState, setCatalogConfig, loadMoreData } from './catalogLoader';
 import { setCategory } from './utils';
 
+let isLoading = false;
+
 export function bindFilterButton(selectedJenis) {
-    $(document).on('click', '.category-filter', function (e) {
+    $(document).on('click', '.category-filter', async function (e) {
         e.preventDefault();
+        if (isLoading) return; // prevent overlapping calls
+        isLoading = true;
+
         const category = $(this).data('id');
 
-        setCatalogConfig({ selectedJenis, category });
-        setCategory(category);
+        try {
+            setCatalogConfig({ selectedJenis, category });
+            setCategory(category);
+            $('#filterModal').modal('hide');
+            resetState();
 
-        $('#filterModal').modal('hide');
-        resetState();
-        loadMoreData();
+            await loadMoreData(); // pastikan ini mengembalikan Promise
+        } catch (err) {
+            console.error('Gagal memuat data:', err);
+        } finally {
+            isLoading = false;
+        }
     });
 }
