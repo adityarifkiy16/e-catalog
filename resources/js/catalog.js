c9520e1c7e947cf1d31461322bc109f3b33a733e
adityarifkiy16 <adityarifkistemba@gmail.com>
2025-08-04 14:08:25 +0700

    fix : perbaikain layout untuk tipe aksesoris

--- resources/js/catalog.js
+++ resources/js/catalog.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
         $('#filter-container').addClass('d-none');
     }
 
-    if (selectedJenis == 1 || selectedJenis == 3) {
+    if (selectedJenis == 1 || selectedJenis == 3 || selectedJenis == 4) {
         $('#filter-container').addClass('d-none');
         $('#catalog-col').removeClass('col-md-10');
         $('#catalog-col').addClass('col-md-12');
