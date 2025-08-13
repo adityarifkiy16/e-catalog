008a2419f2df03877b3cd0b81f17150b42051073
adityarifkiy16 <adityarifkistemba@gmail.com>
2025-08-13 15:00:02 +0700

    change logo 17an

--- resources/views/catalog/catalog.blade.php
+++ resources/views/catalog/catalog.blade.php
@@ -14,7 +14,7 @@
         <div class="w-100 shadow-sm py-3 px-4 d-flex justify-content-between align-items-center bg-black">
             <!-- Logo -->
             <a href="https://osborn.id/" target="_blank" class="d-flex align-items-center">
-                <img src="{{ asset('dist/img/osborn.png') }}v={{ time() }}" alt="osborn-logo"
+                <img src="{{ asset('dist/img/osborn.png') }}?v={{ time() }}" alt="osborn-logo"
                     style="width: 130px; height: auto;">
             </a>
 
