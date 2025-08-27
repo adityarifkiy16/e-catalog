a7e207d5291cbe52682b44f290127b237ed4141b
adityarifkiy16 <adityarifkistemba@gmail.com>
2025-08-27 16:02:09 +0700

    feat : build js video landing page

---
+++ public/build/assets/catalog-CtDIQzX7.js
@@ -0,0 +1,181 @@
+let A=null;function M(e){A=e}function K(){return A}function S(){$("#loading").removeClass("d-none")}function H(){$("#loading").addClass("d-none")}function E(e){return e.slice().sort((o,t)=>o.pivot.motif&&!t.pivot.motif?-1:!o.pivot.motif&&t.pivot.motif?1:0).map(o=>"/storage/"+o.path)}function V(){$(document).on("click",".modalDownload",function(e){e.preventDefault();const o=$(this).data("id"),t="catalog/pdf/product?id="+encodeURIComponent(o);D(this,t)}),$("#btn-download").on("click",function(e){e.preventDefault(),D(this,"catalog/pdf?category="+encodeURIComponent(K()))})}function D(e,o){const t=$(e);t.prop("disabled",!0).html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Mengunduh...'),window.open(o,"_blank"),setTimeout(()=>{t.prop("disabled",!1).html('<i class="fa fa-file-download"></i> Download')},5e3)}function _(){$(document).on("click",".modalContact",function(e){e.preventDefault(),console.log("Klik tombol order",$(this).data("code"));const o="62816659688",t=$(this).data("jenis");console.log(t.toLowerCase());let a="";t.toLowerCase()=="wallpanel"?a=`Halo Admin,
+Saya tertarik dengan produk berikut:
+
+• Tipe Wallpanel: *${$(this).data("type")}*
+• Kode Motif : *${$(this).data("code")}*
+• Produk : *${$(this).data("jenis")}*
+• Kategori: *${$(this).data("category")}*
+
+Apakah produk ini masih tersedia? Terima kasih.`:t.toLowerCase()=="pvc board"?a=`Halo Admin,
+Saya tertarik dengan produk berikut:
+
+• Produk : *${$(this).data("jenis")}*
+• Ketebalan : *${$(this).data("code")}*
+• Kepadatan: *${$(this).data("kepadatan")??"-"}*
+
+Apakah produk ini masih tersedia? Terima kasih.`:t.toLowerCase()=="uv board"?a=`Halo Admin,
+Saya tertarik dengan produk berikut:
+
+• Kode Motif : *${$(this).data("code")}*
+• Produk : *${$(this).data("jenis")}*
+• Kategori: *${$(this).data("category")}*
+
+• Paket: *${$(this).data("kepadatan")??"-"}*
+
+Apakah produk ini masih tersedia? Terima kasih.`:a=`Halo Admin,
+Saya tertarik dengan produk berikut:
+
+• Kode Motif : *${$(this).data("code")}*
+• Produk : *${$(this).data("jenis")}*
+• Kategori: *${$(this).data("category")}*
+
+Apakah produk ini masih tersedia? Terima kasih.`,window.open(`https://wa.me/${o}?text=${encodeURIComponent(a)}`,"_blank")})}function G(){$(window).scroll(function(){$(this).scrollTop()>100?$("#btn-scroll-top").fadeIn():$("#btn-scroll-top").fadeOut()}),$("#btn-scroll-top").click(function(){return $("html, body").animate({scrollTop:0},500),!1})}function U(e,o){let t="";e.forEach(a=>{var d,r,u,p,f,k,w,C,x;const l=a.photo?`/storage/${a.photo}`:"https://via.placeholder.com/300x200?text=No+Image",n=E(a.images),i=[l,...n],c=JSON.stringify(i).replace(/"/g,"&quot;"),s=((d=a.category)==null?void 0:d.name)??"Tanpa Kategori";let m=a.code;o==3?m=a.code.split(" ").slice(4).join(" ").trim():o==5?m=a.code.split(" ").slice(1).join(" "):m=a.code,((r=a.category)==null?void 0:r.display_style)==="square"||o===null?t+=`
+                    <div class="col-md-2 col-6 mb-4">
+                        <div class="h-100 product-card"
+                    `:((u=a.category)==null?void 0:u.display_style)==="rectangle"?t+=`
+                    <div class="col-md-4 col-6 mb-4">
+                        <div class="h-100 product-card"
+                    `:t+=`
+                    <div class="col-md-2 col-6 mb-4">
+                        <div class="h-100 product-card d-flex flex-column justify-content-center align-items-center"
+                    `,t+=`
+                    data-id="${a.id}"
+                    data-code="${m}"
+                    data-category="${s}"
+                    data-jenis="${((f=(p=a.category)==null?void 0:p.jenis)==null?void 0:f.name)??""}"
+                    data-length="${a.panjang}"
+                    data-height="${a.tinggi}"
+                    data-density="${a.ketebalan}"
+                    data-images="${c}"
+                    data-image="${l}"
+                    data-type="${((w=(k=a.category)==null?void 0:k.types)==null?void 0:w.name)??""}"
+                    >
+                       <img 
+                            src="${l}" 
+                            class="card-img-top" 
+                            alt="${a.name}" 
+                            style="
+                                border : 1px solid #2c2c2c;
+                                min-height: 10rem;
+                                aspect-ratio: 1/1;
+                                height: auto; 
+                                border-radius: 8px;
+                                width: 100%; 
+                                object-fit: cover; 
+                                object-position: ${((x=(C=a.category)==null?void 0:C.jenis)==null?void 0:x.name)==="PVC Board"?"bottom center":"center center"};
+                            "
+                        >
+                        <div class="card-body d-flex flex-column text-center">
+                            <h4 class="card-title font-weight-bold text-uppercase mb-2">
+                                ${m}
+                            </h4>
+                            <h6 class="card-text text-muted mb-1">${s}</h6>
+                        </div>
+                    </div>
+                </div>`}),$("#product-list .row").append(t)}function O(e,o){let t="";e.forEach(a=>{const l=a.thumbnail?`/storage/${a.thumbnail}`:"https://via.placeholder.com/300x200?text=No+Image";console.log(o);const n=o==3?"wallpanel":"tanpa kategori";t+=`
+                    <div class="col-md-2 col-6 mb-4">
+                        <div class="h-100 product-card d-flex flex-column justify-content-center align-items-center"
+                    `,t+=`
+                    data-id="${a.id}"
+                    data-jenis="tipe-wallpanel"
+                    data-category="${n}"
+                    data-type="${a.id}"
+                    >
+                       <img 
+                            src="${l}" 
+                            class="card-img-top" 
+                            alt="${a.name}" 
+                            style="
+                                border : 1px solid #2c2c2c;
+                                min-height: 10rem;
+                                aspect-ratio: 1/1;
+                                height: auto; 
+                                border-radius: 8px;
+                                width: 100%; 
+                                object-fit: cover; 
+                            "
+                        >
+                        <div class="card-body d-flex flex-column text-center">
+                            <h4 class="card-title font-weight-bold text-uppercase mb-2">
+                                ${a.name}
+                            </h4>
+                            <h6 class="card-text text-muted mb-1">${n}</h6>
+                        </div>
+                    </div>
+                </div>`}),$("#product-list .row").append(t)}function R(e,o,t){if(o==null)return;e.forEach(c=>{c.path&&t.add(c.path)});const a=Array.from(t),l=$("#mockup-carousel-inner"),n=$("#mockup-carousel-indicators"),i=["https://www.youtube.com/embed/x55DLsMH-lA?autoplay=1&mute=1&rel=0&controls=0&amp;loop=1&playlist=x55DLsMH-lA"];if(l.empty(),n.empty(),o==4){$("#carouselExampleControls").carousel({interval:!1}),$("#mockup .carousel-control-next").addClass("d-none"),$("#mockup .carousel-control-prev").addClass("d-none"),i.forEach((c,s)=>{l.append(`
+                <div class="carousel-item ${s===0?"active":""}">
+                    <div class="d-flex justify-content-center align-items-center">
+                        <div class="embed-responsive embed-responsive-16by9" style="width:100%; height:70vh;">
+                            <iframe
+                                class="embed-responsive-item"
+                                src="${c}"
+                                title="YouTube video player"
+                                frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; web-share;"
+                                referrerpolicy="strict-origin-when-cross-origin"
+                                allowfullscreen 
+                            ></iframe>
+                        </div>
+                    </div>
+                </div>
+            `)}),$("#mockup").removeClass("d-none");return}a.length>0?(a.length===1?($("#mockup .carousel-control-next").addClass("d-none"),$("#mockup .carousel-control-prev").addClass("d-none")):($("#mockup .carousel-control-next").removeClass("d-none"),$("#mockup .carousel-control-prev").removeClass("d-none")),a.slice(0,5).forEach((c,s)=>{l.append(`
+                        <div class="carousel-item ${s===0?"active":""}">
+                            <img src="/storage/${c}" id="mockup-image" alt="mockup" 
+                                class="img-fluid w-100 rounded-lg d-block mx-auto"
+                                style="
+                                    width: 100%;  
+                                    height: 70vh;                
+                                    aspect-ratio: 16 / 9;      
+                                    object-fit: cover;        
+                                    object-position: center 75%;   
+                                    display: block;
+                                    margin: 0 auto;          
+                                    border-radius: 8px;        
+                                    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); 
+                                ">
+                        </div>
+                    `),n.append(`
+                        <li data-target="#carouselExampleControls" data-slide-to="${s}" ${s===0?'class="active"':""}></li>
+                    `)}),$("#mockup").removeClass("d-none")):$("#mockup").addClass("d-none")}let y=null,g=null,N=null,h=1,j=!1,v=!1;const B=new Set;function I(e){y=e.selectedJenis,g=e.category??null,N=e.type??null}function b(e=!0){return new Promise((o,t)=>{if($(window).width()<768&&$("#filter-container").addClass("d-none"),!g||g==="null"||g===""?$("#btn-download").addClass("d-none"):$("#btn-download").removeClass("d-none"),j||v)return o();j=!0,S();const a=$("#search-input").val();$.ajax({url:"/catalog",type:"GET",data:{page:h,search:a,jenis:y,category:g,type:N},success:function(l){console.log("Data berhasil dimuat."),console.log(l);const n=l.data.data??[],i=l.types??[];N&&($("#backButton").removeClass("d-none"),$("#homeButton").addClass("d-none"),console.log("remove class")),y==3&&e?(console.log("isFirstLoad:"+e),i.length>0&&(O(i,y),h++,h>l.data.last_page&&(v=!0)),P(l,!0)):(console.log("isFirstLoad:"+e),n.length>0?(U(n,y),h++,h>l.data.last_page&&(v=!0)):(h===1&&($("#mockup").addClass("d-none"),$("#product-list .row").append(`<div class="col-12"><img src="dist/img/no-data.png" alt="no-data"
+                                class="img-fluid mx-auto d-block" style="max-width: 100%; height: auto; margin-top: 100px; margin-bottom: 100px;"></div>`)),v=!0),P(l,!1)),P(l,!1),o()},error:function(){console.log("Gagal memuat data."),t()},complete:function(){j=!1,H()}})})}function P(e,o=!0){var c,s,m;const t=e.category,a=(c=e.jenis)==null?void 0:c.name,n=((m=(s=e.data.data[0])==null?void 0:s.category)==null?void 0:m.images)??[];switch(t.length===0?$("#category-container").addClass("d-none"):($("#category-container").removeClass("d-none"),n.length===0&&$("#mockup").addClass("d-none"),R(n,y,B)),a){case"PVC Board":$("#category-container, #category-modal-container").addClass("d-none");break;case"Wallboard":case"UV Board":$("#category-menu-item-label, #category-modal-item-label").html("Motif");break;case"Wallpanel":break;case"Aksesoris":$("#category-container, #category-modal-container").addClass("d-none");break;default:$("#category-menu-item-label, #category-modal-item-label").html("Kategori")}let i='<li class="nav-item font-poppins">';Array.isArray(t)&&t.length>0?t.forEach(d=>{i+=`
+            <a class="nav-link text-white category-filter d-flex align-items-center justify-content-start" 
+                href="#" data-jenis-id="${d.jenis_id}" data-id="${d.id}" data-type="${d.type_id}">
+                <img src="${d.path?"storage/"+d.path:"dist/img/product/1.webp"}" alt="${d.name}" 
+                class="mr-2 img-thumbnail" style="width: 50px; height: 50px; object-fit: contain;">
+                <span class="text-capitalize">${d.name}</span>
+            </a>`}):i+='<a class="nav-link text-white category-filter" href="#">Tanpa Kategori</a>',i+="</li>",$("#category-menu-item, #category-menu-item-modal").html(i),!g&&t.length>0?(g=t[0].id,M(g),T(),b(o)):g&&$(`.category-filter[data-id="${g}"]`).addClass("active")}function T(){h=1,j=!1,v=!1,B.clear(),$("#product-list .row").html("")}let L=!1;function W(e){$(document).off("click",".category-filter"),$(document).on("click",".category-filter",async function(o){if(o.preventDefault(),L)return;L=!0;const t=$(this).data("id"),a=$(this).data("type");console.log(a);try{I({selectedJenis:e,category:t,type:a}),M(t),$("#filterModal").modal("hide"),T(),await b(!1)}catch(l){console.error("Gagal memuat data:",l)}finally{L=!1}})}$(document).ready(function(){let e=new URLSearchParams(window.location.search).get("jenis"),o=new URLSearchParams(window.location.search).get("category"),t,a;(e==5||e==2)&&sessionStorage.removeItem("selectedWallpanel"),I({selectedJenis:e,category:o}),b(),e&&($("#category-container").removeClass("d-md-none"),$("#catalog-col").removeClass("center-content")),$(window).width()<768&&$("#filter-container").addClass("d-none"),(e==1||e==3||e==4)&&($("#filter-container").addClass("d-none"),$("#catalog-col").removeClass("col-md-10"),$("#catalog-col").addClass("col-md-12")),$("#search-input").on("input",function(){clearTimeout(t),t=setTimeout(()=>{T(),b()},500)}),$(window).on("scroll",function(){clearTimeout(a),a=setTimeout(()=>{const n=$(window).scrollTop(),i=$(window).height(),c=$(document).height();n+i>=c-150&&b(!1)},200)});function l(n,i=null,c=null){$("#carousel-product-image").empty(),$("#thumbnailGallery").empty();let s=[...n];c.toLowerCase()=="uv board"?s=[...n,...i]:s=[...n],s.forEach((d,r)=>{const u=r===0?"active":"";$("#carousel-product-image").append(`
+                    <div class="carousel-item ${u}">
+                        <img src="${d}" class="img-fluid d-block mx-auto"
+                            style="
+                                width: 100%;
+                                max-width: 400px;
+                                aspect-ratio: 1 / 1;
+                                object-fit: cover;
+                                border-radius: 8px;
+                                border: 1px solid #ccc;
+                            ">
+                    </div>
+                `),$("#thumbnailGallery").append(`
+                    <div class="col-2 mb-0 d-flex justify-content-center">
+                        <div style="height: 90%">
+                           <img src="${d}" 
+                            class="img-thumbnail thumbnail-image p-0 w-100 h-100" 
+                            style="
+                                height: auto;
+                                aspect-ratio: 1 / 1;
+                                border: 1px solid #ccc;
+                                border-radius: 8px;
+                                object-fit: cover;
+                                cursor: pointer;"
+                            data-index="${r}">
+                        </div>
+                    </div>
+                    `)}),n.length<=1?($("#carouselProduct .carousel-control-next").addClass("d-none"),$("#carouselProduct .carousel-control-prev").addClass("d-none")):($("#carouselProduct .carousel-control-next").removeClass("d-none"),$("#carouselProduct .carousel-control-prev").removeClass("d-none")),$("#carouselProduct").carousel({interval:3e3,pause:!1});let m;$("#thumbnailGallery").off("click").on("click",".thumbnail-image",function(){const d=$(this).data("index");$("#carouselProduct").carousel(d),$(".thumbnail-image").removeClass("active-thumbnail"),$(this).addClass("active-thumbnail"),setTimeout(()=>{$(this).removeClass("active-thumbnail")},500),$("#carouselProduct").carousel("pause"),clearTimeout(m),m=setTimeout(()=>{$("#carouselProduct").carousel("cycle")},5e3)})}$(document).on("click",".product-card",function(){const n=$(this).data("code"),i=$(this).data("category"),c=$(this).attr("data-images"),s=$(this).data("type");let m=null,d=[{path:`/dist/img/paket/1.png?v=${Date.now()}`},{path:`/dist/img/paket/2.png?v=${Date.now()}`},{path:`/dist/img/paket/3.png?v=${Date.now()}`}];d=d.map(x=>x.path),c&&(m=JSON.parse(c.replace(/&quot;/g,'"')));const r=$(this).data("jenis"),u=parseInt($(this).data("length"),10),p=parseInt($(this).data("height"),10),f=parseInt($(this).data("density"),10),k=$(this).data("id");if(r==="tipe-wallpanel"){T(),I({selectedJenis:3,type:s}),b(!1),$("#filter-container").toggleClass("d-none"),$("#catalog-col").toggleClass("col-md-10 col-md-12");return}r.toLowerCase()==="uv board"&&($("#ketebalan, #kepadatan,  #modalVideo").hide(),$("#modalCategory").text(i),$("#modalLength").text(u&&!isNaN(u)?u+" cm":"-"),$("#modalHeight").text(p&&!isNaN(p)?p+" cm":"-"),$("#modalPaket").html(`
+                <span class="badge badge-outline-primary px-2 py-1 kepadatan" data-value="1">Bundle 1</span>
+                <span class="badge badge-outline-primary px-2 py-1 kepadatan" data-value="2">Bundle 2</span>
+                <span class="badge badge-outline-primary px-2 py-1 kepadatan" data-value="3">Bundle 3</span>
+            `)),r.toLowerCase()==="wallboard"&&($("#modalCategory").text(i),$("#modalLength").text(u&&!isNaN(u)?u+" cm":"-"),$("#modalHeight").text(p&&!isNaN(p)?p+" cm":"-"),$("#ketebalan, #kepadatan, #notes, .paket,  #modalVideo").hide()),r==="PVC Board"&&($("#modalCategory").text(r),$("#modalLength").text(u&&!isNaN(u)?u+" cm":"-"),$("#modalHeight").text(p&&!isNaN(p)?p+" cm":"-"),$("#modalDensity").text(f&&!isNaN(f)?f+" mm":"-"),$("#modalKepadatan").html(`
+                <span class="badge badge-outline-primary kepadatan" data-value="0,4 mm">0,4 mm</span>
+                <span class="badge badge-outline-primary kepadatan" data-value="0,55 mm">0,55 mm</span>
+                <span class="badge badge-outline-primary kepadatan" data-value="0,7 mm">0,7 mm</span>
+            `),$(".paket,  #modalVideo").hide()),r==="Wallpanel"&&($("#modalContact").data("type",s),$("#modalCategory").text(i+" / "+s),$("#panjang, #tinggi, #ketebalan, #kepadatan, #notes, .paket, #modalVideo").hide()),r.toLowerCase()==="aksesoris"&&($("#panjang, #tinggi, #ketebalan, #kepadatan, #notes, .paket").hide(),$("#modalCategory").text(i)),l(m,d,r),$("#modalCode").text(n),$("#productModalLabel").text(n),$("#modalDownload").data("id",k),$("#productModal").modal("show"),$("#modalContact").data("jenis",r),$("#modalContact").data("category",i),$("#modalContact").data("code",n);let w=$("#modalContact").data("kepadatan"),C=$("#modalContact").data("paket");w&&($(".kepadatan").removeClass("active"),$("#modalContact").data("kepadatan",null),$("#notes").show()),C&&($(".kepadatan").removeClass("active"),$("#modalContact").data("kepadatan",null),$("#notes").show())}),$(document).on("click",".kepadatan",function(){let n=$(this).data("value");$(this).closest("#productModal").find(".kepadatan").removeClass("active"),$(this).addClass("active"),$("#notes").hide(),$("#modalContact").data("kepadatan",n)}),W(e),V(),_(),G()});
