4fb79f0005b84bfa998bc9a1e976cd6cfe12f51d
adityarifkiy16 <adityarifkistemba@gmail.com>
2025-09-18 15:38:29 +0700

    feat : add lebar on table product and seeder

---
+++ database/seeders/SizeWallpanelSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\TProduct;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class SizeWallpanelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $sizes = [
+            ['type_id' => 1, 'panjang' => 300, 'tinggi' => 9, 'lebar' => 20.7],
+            ['type_id' => 2, 'panjang' => 300, 'tinggi' => 24, 'lebar' => 17],
+            ['type_id' => 3, 'panjang' => 300, 'tinggi' => 15, 'lebar' => 21.5],
+            ['type_id' => 4, 'panjang' => 300, 'tinggi' => 8, 'lebar' => 16.7],
+            ['type_id' => 5, 'panjang' => 300, 'tinggi' => 14.2, 'lebar' => 20.7],
+            ['type_id' => 6, 'panjang' => 300, 'tinggi' => 14, 'lebar' => 15.5],
+        ];
+
+        foreach ($sizes as $size) {
+            TProduct::whereHas('category', function ($query) use ($size) {
+                $query->where('type_id', $size['type_id']);
+            })->update([
+                'panjang' => $size['panjang'],
+                'tinggi'  => $size['tinggi'],
+                'lebar'   => $size['lebar'],
+            ]);
+        }
+    }
+}
