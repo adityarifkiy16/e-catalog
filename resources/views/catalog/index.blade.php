a58d1b9776d4fedc267f41c8d2112865afb8d2eb
adityarifkiy16 <adityarifkistemba@gmail.com>
2025-09-19 15:20:12 +0700

    refactor : change version web

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -345,7 +345,7 @@
                         </li>
                     </ul>
                     <p class="mt-5 mb-1">&copy; 2025 Osborn. All rights reserved.</p>
-                    <span class="text-muted">Updated: 28 Agustus 2025 | v1.6</span>
+                    <span class="text-muted">Updated: 19 September 2025 | v1.7</span>
 
                 </div>
             </div>
