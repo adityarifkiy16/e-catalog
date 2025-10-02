ec1402dd23165e0094894484f8b661f29640d225
aditya <adityarifkistemba@gmail.com>
2025-10-02 17:09:40 +0700

    fix : hide density on aksesoris modal

--- resources/js/catalog.js
+++ resources/js/catalog.js
@@ -229,7 +229,7 @@ $(document).ready(function () {
             renderVariantsToModal(variants);
 
             $('#modalVideoPlayer').empty();
-            $('#modalVideo').hide();
+            $('#modalVideo, .density').hide();
             if (urlVideo) {
                 $('#modalVideoPlayer').append(`
                     <iframe class="embed-responsive-item"
