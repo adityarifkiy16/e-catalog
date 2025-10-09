dcc8e2559b26202882a32b58add60e01b388dec1
adityarifkiy16 <adityarifkistemba@gmail.com>
2025-10-09 13:04:27 +0700

    fix : add activate sidebar

--- resources/views/partials/sidebar/_nav_item.blade.php
+++ resources/views/partials/sidebar/_nav_item.blade.php
@@ -1,4 +1,4 @@
-<li class="nav-item">
+<li class="nav-item {{ request()->routeIs($route) ? 'menu-open' : '' }}">
     <a href="{{ route($route) }}" class="nav-link {{ request()->routeIs($route) ? 'bg-primary' : 'bg-dark' }}">
         <i class="{{ $icon }} nav-icon"></i>
         <p>{{ $label }}</p>
--- resources/views/partials/sidebar/_nav_tree.blade.php
+++ resources/views/partials/sidebar/_nav_tree.blade.php
@@ -8,7 +8,7 @@
     </a>
     <ul class="nav nav-treeview pl-4">
         @foreach ($items as $item)
-            <li class="nav-item">
+            <li class="nav-item {{ request()->routeIs($item['route']) ? 'bg-primary' : '' }}">
                 <a href="{{ route($item['route']) }}" class="nav-link" style="">
                     <i class="{{ $item['icon'] ?? 'fas fa-circle' }} nav-icon"></i>
                     <p>{{ $item['label'] }}</p>
