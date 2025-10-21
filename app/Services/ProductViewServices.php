48abe512243789e4e253d0c8709c2a4371fd5f21
aditya <adityarifkistemba@gmail.com>
2025-10-21 10:25:08 +0700

    fix : handling race conditions

--- app/Services/ProductViewServices.php
+++ app/Services/ProductViewServices.php
@@ -2,9 +2,10 @@
 
 namespace App\Services;
 
-use App\Models\ProductView;
 use App\Models\TProduct;
+use App\Models\ProductView;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Cache;
 
 class ProductViewServices
@@ -15,16 +16,17 @@ class ProductViewServices
         $ipAddress = $request->ip();
         $userAgent = $request->userAgent();
         $cacheKey = "product_view_{$product->id}_{$ipAddress}";
+
         if (Cache::has($cacheKey)) {
             return;
         }
 
-        ProductView::create([
+        ProductView::updateOrCreate([
             'product_id' => $product->id,
             'ip_address' => $ipAddress,
             'user_agent' => $userAgent,
             'viewed_at' => now(),
-        ]);
+        ], ['viewed_at' => now()]);
 
         Cache::put($cacheKey, true, now()->addDay());
 
@@ -33,11 +35,37 @@ class ProductViewServices
 
     public function filter($filter)
     {
-        $query = TProduct::withCount(['views as views_count' => function ($q) use ($filter) {
-            $q->where('viewed_at', '>=', now()->subDays($filter));
-        }])
+        $query = TProduct::withCount([
+            'views as views_count' => function ($q) use ($filter) {
+                if ($filter) {
+                    $filter = trim($filter);
+
+                    // Jika mengandung rentang (contoh: 2025-10-01 - 2025-10-21)
+                    if (str_contains($filter, ' - ')) {
+                        [$start, $end] = array_map('trim', explode(' - ', $filter));
+
+                        try {
+                            $start = \Carbon\Carbon::parse($start)->startOfDay();
+                            $end =  \Carbon\Carbon::parse($end)->endOfDay();
+                            $q->whereBetween('viewed_at', [$start, $end]);
+                        } catch (\Exception $e) {
+                            Log::warning('Filter tanggal tidak valid: ' . $filter);
+                        }
+                    } else {
+                        // Jika hanya 1 tanggal
+                        try {
+                            $date =  \Carbon\Carbon::parse($filter)->toDateString();
+                            $q->whereDate('viewed_at', $date);
+                        } catch (\Exception $e) {
+                            Log::warning('Filter tanggal tunggal tidak valid: ' . $filter);
+                        }
+                    }
+                }
+            }
+        ])
+            ->having('views_count', '>', 0)
             ->orderBy('views_count', 'desc')
-            ->limit(5);
+            ->limit(10);
 
         return $query;
     }
