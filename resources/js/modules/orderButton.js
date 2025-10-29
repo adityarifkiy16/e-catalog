5ba9dd24d27af4ce8f2f46f41ab1b0fcc1f58cbd
aditya <adityarifkistemba@gmail.com>
2025-10-29 15:42:08 +0700

    fix : double tab open whatsapp

--- resources/js/modules/orderButton.js
+++ resources/js/modules/orderButton.js
@@ -1,53 +1,55 @@
 export function bindOrderButton() {
-    $(document).on('click', '.modalContact', function (e) {
-        e.preventDefault();
-        console.log('Klik tombol order', $(this).data('code'));
-        const phone = '62816659688';
-        const jenis = $(this).data('jenis');
-        console.log(jenis.toLowerCase());
-        let message = '';
-        if (jenis.toLowerCase() == 'wallpanel') {
-            message =
-                `Halo Admin,\nSaya tertarik dengan produk berikut:\n\n` +
-                `• Tipe Wallpanel: *${$(this).data('type')}*\n` +
-                `• Kode : *${$(this).data('code')}*\n` +
-                `• panjang : *${$(this).data('length')} cm*\n` +
-                `• tinggi : *${$(this).data('height')} cm*\n` +
-                `• lebar : *${$(this).data('width')} cm*\n` +
-                `• Produk : *${$(this).data('jenis')}*\n` +
-                `• Kategori: *${$(this).data('category')}*\n\n` +
-                `Apakah produk ini masih tersedia? Terima kasih.`;
-        } else if (jenis.toLowerCase() == 'pvc board') {
-            message =
-                `Halo Admin,\nSaya tertarik dengan produk berikut:\n\n` +
-                `• Produk : *${$(this).data('jenis')}*\n` +
-                `• Ketebalan : *${$(this).data('code')}*\n` +
-                `• density: *${$(this).data('kepadatan') ?? '-'}*\n\n` +
-                `Apakah produk ini masih tersedia? Terima kasih.`;
-        } else if (jenis.toLowerCase() == 'uv board') {
-            message =
-                `Halo Admin,\nSaya tertarik dengan produk berikut:\n\n` +
-                `• Kode Motif : *${$(this).data('code')}*\n` +
-                `• Produk : *${$(this).data('jenis')}*\n` +
-                `• Kategori: *${$(this).data('category')}*\n\n` +
-                `• Paket: *${$(this).data('kepadatan') ?? '-'}*\n\n` +
-                `Apakah produk ini masih tersedia? Terima kasih.`;
-        } else if (jenis.toLowerCase() == 'aksesoris') {
-            message =
-                `Halo Admin,\nSaya tertarik dengan produk berikut:\n\n` +
-                `• Kode : *${$(this).data('code')}*\n` +
-                `• Produk : *${$(this).data('jenis')}*\n` +
-                `• Ukuran : *${$(this).data('category')}*\n\n` +
-                `• Warna : *${$(this).data('kepadatan')}*\n\n` +
-                `Apakah produk ini masih tersedia? Terima kasih.`;
-        } else {
-            message =
-                `Halo Admin,\nSaya tertarik dengan produk berikut:\n\n` +
-                `• Kode Motif : *${$(this).data('code')}*\n` +
-                `• Produk : *${$(this).data('jenis')}*\n` +
-                `• Kategori: *${$(this).data('category')}*\n\n` +
-                `Apakah produk ini masih tersedia? Terima kasih.`;
-        }
-        window.open(`https://wa.me/${phone}?text=${encodeURIComponent(message)}`, '_blank');
-    });
+    $(document)
+        .off('click', '.modalContact')
+        .on('click', '.modalContact', function (e) {
+            e.preventDefault();
+            console.log('Klik tombol order', $(this).data('code'));
+            const phone = '62816659688';
+            const jenis = $(this).data('jenis');
+            console.log(jenis.toLowerCase());
+            let message = '';
+            if (jenis.toLowerCase() == 'wallpanel') {
+                message =
+                    `Halo Admin,\nSaya tertarik dengan produk berikut:\n\n` +
+                    `• Tipe Wallpanel: *${$(this).data('type')}*\n` +
+                    `• Kode : *${$(this).data('code')}*\n` +
+                    `• panjang : *${$(this).data('length')} cm*\n` +
+                    `• tinggi : *${$(this).data('height')} cm*\n` +
+                    `• lebar : *${$(this).data('width')} cm*\n` +
+                    `• Produk : *${$(this).data('jenis')}*\n` +
+                    `• Kategori: *${$(this).data('category')}*\n\n` +
+                    `Apakah produk ini masih tersedia? Terima kasih.`;
+            } else if (jenis.toLowerCase() == 'pvc board') {
+                message =
+                    `Halo Admin,\nSaya tertarik dengan produk berikut:\n\n` +
+                    `• Produk : *${$(this).data('jenis')}*\n` +
+                    `• Ketebalan : *${$(this).data('code')}*\n` +
+                    `• density: *${$(this).data('kepadatan') ?? '-'}*\n\n` +
+                    `Apakah produk ini masih tersedia? Terima kasih.`;
+            } else if (jenis.toLowerCase() == 'uv board') {
+                message =
+                    `Halo Admin,\nSaya tertarik dengan produk berikut:\n\n` +
+                    `• Kode Motif : *${$(this).data('code')}*\n` +
+                    `• Produk : *${$(this).data('jenis')}*\n` +
+                    `• Kategori: *${$(this).data('category')}*\n\n` +
+                    `• Paket: *${$(this).data('kepadatan') ?? '-'}*\n\n` +
+                    `Apakah produk ini masih tersedia? Terima kasih.`;
+            } else if (jenis.toLowerCase() == 'aksesoris') {
+                message =
+                    `Halo Admin,\nSaya tertarik dengan produk berikut:\n\n` +
+                    `• Kode : *${$(this).data('code')}*\n` +
+                    `• Produk : *${$(this).data('jenis')}*\n` +
+                    `• Ukuran : *${$(this).data('category')}*\n\n` +
+                    `• Warna : *${$(this).data('kepadatan')}*\n\n` +
+                    `Apakah produk ini masih tersedia? Terima kasih.`;
+            } else {
+                message =
+                    `Halo Admin,\nSaya tertarik dengan produk berikut:\n\n` +
+                    `• Kode Motif : *${$(this).data('code')}*\n` +
+                    `• Produk : *${$(this).data('jenis')}*\n` +
+                    `• Kategori: *${$(this).data('category')}*\n\n` +
+                    `Apakah produk ini masih tersedia? Terima kasih.`;
+            }
+            window.open(`https://wa.me/${phone}?text=${encodeURIComponent(message)}`, '_blank');
+        });
 }
