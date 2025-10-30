ae32d475d4d14f08ef55786c89f5cb39ad3350d7
aditya <adityarifkistemba@gmail.com>
2025-10-30 11:01:59 +0700

    fix : change name panjang to lebar uv seeder

--- database/seeders/VarianUVSeeder.php
+++ database/seeders/VarianUVSeeder.php
@@ -11,7 +11,7 @@ class VarianUVSeeder extends Seeder
 {
     public function run(): void
     {
-        $uvboard = ['panjang' => 122, 'tinggi' => 300, 'ketebalan' => 8, 'density' => 0.9];
+        $uvboard = ['lebar' => 122, 'tinggi' => 300, 'ketebalan' => 8, 'density' => 0.9];
 
         foreach ($uvboard as $attrName => $attrValue) {
             $specification = MSpecification::firstOrCreate([
@@ -27,6 +27,8 @@ class VarianUVSeeder extends Seeder
             $product = TProduct::whereHas('category.jenis', fn($q) => $q->where('id', 5))->get();
 
             foreach ($product as $p) {
+                $panjangSpec = MSpecification::where('name', 'panjang')->where('jenis_id', 5)->first();
+                $p->specifications()->detach($panjangSpec->id);
                 $p->specifications()->syncWithoutDetaching([
                     $specification->id => ['specification_value_id' => $specificationValue->id]
                 ]);
