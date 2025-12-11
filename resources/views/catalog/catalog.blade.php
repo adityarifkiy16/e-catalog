4fe3e82671f9a9903d8360bdad647e652672719a
aditya <adityarifkistemba@gmail.com>
2025-12-11 13:22:56 +0700

    fix : handling empty filter

--- resources/views/catalog/catalog.blade.php
+++ resources/views/catalog/catalog.blade.php
@@ -85,18 +85,21 @@
                                     </div>
                                 </div>
                                 <!-- filter versi -->
-                                <div class="mb-2 mb-md-0 text-center text-md-right mr-2 order-1" id="version-filter">
-                                    <select id="version-select" class="form-control bg-danger text-white border-secondary">
-                                        @forelse ($versions as $v)
-                                            <option value="{{ $v->id }}"
-                                                {{ request('version_id') == $v->id ? 'selected' : '' }}>
-                                                Versi {{ $v->version }}
-                                            </option>
-                                        @empty
-                                            <option value="" disabled>Tidak ada versi</option>
-                                        @endforelse
-                                    </select>
-                                </div>
+                                @if (!empty($version))
+                                    <div class="mb-2 mb-md-0 text-center text-md-right mr-2 order-1" id="version-filter">
+                                        <select id="version-select"
+                                            class="form-control bg-danger text-white border-secondary">
+                                            @forelse ($versions as $v)
+                                                <option value="{{ $v->id }}"
+                                                    {{ request('version_id') == $v->id ? 'selected' : '' }}>
+                                                    Versi {{ $v->version }}
+                                                </option>
+                                            @empty
+                                                <option value="" disabled>Tidak ada versi</option>
+                                            @endforelse
+                                        </select>
+                                    </div>
+                                @endif
                             </div>
                         </div>
 
