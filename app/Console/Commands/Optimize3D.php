a3ed4bfef666d1759e221f3ebea2fc111010a449
aditya <110983962+adityarifkiy16@users.noreply.github.com>
2026-01-08 16:32:15 +0700

    fix: optimize3d

--- app/Console/Commands/Optimize3D.php
+++ app/Console/Commands/Optimize3D.php
@@ -29,6 +29,15 @@ class Optimize3D extends Command
     {
         $files = Storage::disk('public')->allFiles('images/3Dcategories');
 
+        $this->optimize($files);
+
+        $files2 = Storage::disk('public')->allFiles('images/categories');
+
+        $this->optimize($files2);
+    }
+
+    protected function optimize($files)
+    {
         foreach ($files as $file) {
             $base = basename($file);
 
@@ -39,28 +48,14 @@ class Optimize3D extends Command
 
             // ===== OVERWRITE ORIGINAL =====
             Image::make($fullPath)
-                ->resize(51, 51, fn($c) => $c->aspectRatio())
+                ->resize(51, 51, function ($constraint) {
+                    $constraint->aspectRatio();
+                    $constraint->upsize();
+                })
                 ->encode('webp', 70)
                 ->save($fullPath);
         }
 
-        $files = Storage::disk('public')->allFiles('images/categories');
-
-        foreach ($files as $file) {
-            $base = basename($file);
-
-            if (!str_ends_with($base, '.webp')) continue;
-
-            $fullPath = storage_path('app/public/' . $file);
-            if (!file_exists($fullPath)) continue;
-
-            // ===== OVERWRITE ORIGINAL =====
-            Image::make($fullPath)
-                ->resize(51, 51, fn($c) => $c->aspectRatio())
-                ->encode('webp', 70)
-                ->save($fullPath);
-        }
-
-        $this->info("\nAll folders optimized successfully!");
+        return $this->info("\nAll folders optimized successfully!");
     }
 }
