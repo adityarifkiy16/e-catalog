9861ea1af98fd1798916dd014ca245d879bf42b1
aditya <adityarifkistemba@gmail.com>
2026-01-09 15:26:24 +0700

    fix : update image optimization logic to enhance quality and remove unnecessary code

--- app/Console/Commands/OptimizeAllImages.php
+++ app/Console/Commands/OptimizeAllImages.php
@@ -15,36 +15,54 @@ class OptimizeAllImages extends Command
     {
         $files = Storage::disk('public')->allFiles('images/products');
 
+        // foreach ($files as $file) {
+        //     $base = basename($file);
+
+        //     if (!str_ends_with($base, '.webp')) continue;
+        //     if (str_ends_with($base, '-164.webp')) continue;
+
+        //     $fullPath = storage_path('app/public/' . $file);
+        //     if (!file_exists($fullPath)) continue;
+
+        //     $dir  = dirname($file);
+        //     $name = pathinfo($file, PATHINFO_FILENAME);
+
+        //     // ===== THUMB 164 =====
+        //     Image::make($fullPath)
+        //         ->resize(164, 164, function ($c) {
+        //             $c->aspectRatio();
+        //             $c->upsize();
+        //         })
+        //         ->encode('webp', 100)
+        //         ->save(storage_path("app/public/{$dir}/{$name}-164.webp"));
+
+        //     // ===== OVERWRITE ORIGINAL =====
+        //     Image::make($fullPath)
+        //         ->resize(800, 800, function ($c) {
+        //             $c->aspectRatio();
+        //             $c->upsize();
+        //         })
+        //         ->encode('webp', 70)
+        //         ->save($fullPath);
+        // }
+
         foreach ($files as $file) {
             $base = basename($file);
-
             if (!str_ends_with($base, '.webp')) continue;
-            if (str_ends_with($base, '-164.webp')) continue;
+            if (!str_ends_with($base, '-164.webp')) continue;
 
             $fullPath = storage_path('app/public/' . $file);
             if (!file_exists($fullPath)) continue;
 
-            $dir  = dirname($file);
-            $name = pathinfo($file, PATHINFO_FILENAME);
-
-            // ===== THUMB 164 =====
             Image::make($fullPath)
                 ->resize(164, 164, function ($c) {
                     $c->aspectRatio();
                     $c->upsize();
                 })
-                ->encode('webp', 70)
-                ->save(storage_path("app/public/{$dir}/{$name}-164.webp"));
-
-            // ===== OVERWRITE ORIGINAL =====
-            Image::make($fullPath)
-                ->resize(800, 800, function ($c) {
-                    $c->aspectRatio();
-                    $c->upsize();
-                })
-                ->encode('webp', 70)
+                ->encode('webp', 100)
                 ->save($fullPath);
         }
+
         $this->info("\nAll folders optimized successfully!");
     }
 }
