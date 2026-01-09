7ca14586855b1dcf31eb320c017f6c315a81bc21
aditya <adityarifkistemba@gmail.com>
2026-01-09 09:01:39 +0700

    fix : srcset

--- resources/views/catalog/catalog.blade.php
+++ resources/views/catalog/catalog.blade.php
@@ -127,16 +127,16 @@
                                             @endphp
                                             <div class="carousel-item {{ $key == 0 ? 'active' : '' }}">
                                                 <div class="rounded-lg overflow-hidden">
-                                                    <img src="{{ $image517 }}" class="mockup-image img-fluid w-100"
+                                                    <img src="{{ $image517 }}" class="mockup-image"
                                                         alt="Mockup {{ $item->name }}"
                                                         srcset="{{ $image517 }} 517w, {{ $original }} 1200w"
-                                                        sizes="(max-width: 845px) 517px, 1200px" width="845"
+                                                        sizes="(max-width: 768px) 100vw, 1200vw" width="845"
                                                         height="470" decoding="async"
                                                         @if ($key == 0) fetchpriority="high" 
-                 loading="eager"
-             @else
-                 loading="lazy" 
-                 fetchpriority="low" @endif>
+                                                            loading="eager"
+                                                        @else
+                                                            loading="lazy" 
+                                                            fetchpriority="low" @endif>
                                                 </div>
                                             </div>
                                         @endforeach
