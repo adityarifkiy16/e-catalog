6f786424cb1280ae537fc94fed4a45e1f1ac3767
aditya <adityarifkistemba@gmail.com>
2026-01-22 09:46:55 +0700

    fix: update breadcrumb labels for clarity in bulk create mockup

--- resources/views/productVersion/bulk-create-mockup.blade.php
+++ resources/views/productVersion/bulk-create-mockup.blade.php
@@ -3,8 +3,7 @@
     <div class="card d-flex px-4 py-2" style="border-radius: 1rem;">
         <x-breadcrumb :items="[
             ['label' => 'Home', 'url' => route('dashboard')],
-            ['label' => 'Paket', 'url' => route('package.index')],
-            ['label' => 'Tambah'],
+            ['label' => 'Upload Desain', 'url' => route('product-versions.bulk.create-motif')],
         ]">
         </x-breadcrumb>
     </div>
